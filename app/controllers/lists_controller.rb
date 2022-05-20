6358d1df1c6415a4c33126ebfb8da424f013a5dc
kihopopo <104831865+kihopopo@users.noreply.github.com>
2022-05-20 09:58:48 +0000

    [Update]3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,17 +1,29 @@
 class ListsController < ApplicationController
-  def new
-    
-     @list = List.new
-  end
 
+  def new
+    @list = List.new
+  end
+  
+   def create
+    list = List.new(list_params)
+    list.save
+    # redirect_to '/top' を削除して、以下コードに変更
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)  
+  end
+  
   def index
     @lists = List.all  
   end
   
-  private
   
+  def show
+    @list = List.find(params[:id])  
+  end
+  
+  private
+  # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body)
   end
-  
 end
